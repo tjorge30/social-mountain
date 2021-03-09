32bb39174c06503f96f75fc11b1b2dac23c46b5e
tjorge30 <mrs.javalera@gmail.com>
2021-03-08 20:48:38 -0800

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './App.css';
 
+import Post from './Post/Post'
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+
+//Base URL: practiceapi.devmountain.com/api
 
 class App extends Component {
   constructor() {
@@ -19,18 +22,35 @@ class App extends Component {
   }
   
   componentDidMount() {
-
+    axios.get('https://practiceapi.devmountain.com/api/posts')
+    .then( results => {
+      this.setState({ posts: results.data })
+    .catch(error => console.log(error));
+    });
   }
 
   updatePost() {
-  
+    axios.put(`https://practiceapi.devmountain.com/api/posts?id=${ id }`, { text })
+    .then( results => {
+      this.setState({ posts: results.data })
+    .catch(error => console.log(error));
+    });
   }
 
   deletePost() {
-
+    axios.delete(`https://practiceapi.devmountain.com/api/posts?id=${ id }`)
+    .then( results => {
+      this.setState({ posts: results.data })
+    .catch(error => console.log(error));
+    });
   }
 
   createPost() {
+    axios.post('https://practiceapi.devmountain.com/api/posts', { text })
+    .then( results => {
+      this.setState({ posts: results.data })
+    .catch(error => console.log(error));
+    });
 
   }
 
@@ -44,7 +64,17 @@ class App extends Component {
         <section className="App__content">
 
           <Compose />
-          
+
+          {
+            posts.map( post => (
+              <Post key={ post.id }
+                    text={ post.text}
+                    date={ post.date }
+                    id={ post.id }
+                    updatePostFn={ this.updatePost }
+                    deletePostFn={ this.deletePost } />
+            ))
+          }
         </section>
       </div>
     );
